e6ce2a7535a407db7eb4ac342e8da244b7372775
Pedro Flores <armandorf.05@gmail.com>
2016-01-23 23:44:48 -0800

    Add switch statement for handling pressed keys

--- js/app.js
+++ js/app.js
@@ -29,16 +29,39 @@ var Player = function() {
 };
 
 Player.prototype.update = function(dt) {
-      
-};
-
-Player.prototype.render = function() {
     
 };
 
+Player.prototype.render = function() {
+
+};
+
+Player.prototype.handleInput = function(keyPressed) {
+    // when a valid key is pressed, handle action for key pressed
+    if (keyPressed) {
+        switch (keyPressed) {
+            case 'left':
+                // move the player to the block left of him
+                console.log('left key pressed!');
+                break;
+            case 'up':
+                break;
+            case 'right':
+                break;
+            case 'down':
+                break;
+        }
+    }
+    // else, do nothing
+}
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+// TODO: create an n number of enemies and add them the the array
 var allEnemies = [];
+allEnemies.push(new Enemy());
+
+
 
 // Place the player object in a variable called player
 var player = new Player();
